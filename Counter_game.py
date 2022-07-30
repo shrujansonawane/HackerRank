8e453126a1a8ea22a563f897a5ff2e108fd9fc45
Shrujan Sonawane <87773696+shrujansonawane@users.noreply.github.com>
2022-07-30 18:47:51 +0530

    Create Counter_game.py

---
+++ Counter_game.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'counterGame' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def counterGame(n):
+    countbit=bin(n-1).count('1')
+    if(countbit%2==1):
+        return "Louise"
+    else:
+        return "Richard"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = counterGame(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
